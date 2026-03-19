902e4ee9112101d1036e30142c4bef2404e6a58f
Extrator34 <116797725+Extrator34@users.noreply.github.com>
2026-03-19 15:26:23 +0100

    Create canon.js

---
+++ canon.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const canonSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  category: { type: String, required: true },
+  assignedTo: { type: String, default: null }
+});
+
+export default mongoose.model("Canon", canonSchema);
