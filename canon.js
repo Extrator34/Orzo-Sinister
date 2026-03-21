aa1156883a3b2af108a4034d899d9acc303d097d
Extrator34 <116797725+Extrator34@users.noreply.github.com>
2026-03-21 16:22:48 +0100

    Update canon.js

--- canon.js
+++ canon.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const canonSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   category: { type: String, required: true },
-  subCategory: { type: String, required: true },  
+  subCategory: { type: String, required: true },
+  order: { type: Number, required: true },
   assignedTo: { type: String, default: null }
 });
 
